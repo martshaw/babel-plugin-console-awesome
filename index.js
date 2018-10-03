64fc2fac518049135a3056af67bd1bb42917a688
Travis CI <builds@travis-ci.com>
2018-10-03 20:55:47 +0200

    adding babel plugin code

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-exports.consoleAwesome = function() {
-  console.log("This will be awesome");
-}
+module.exports = function() {
+    return {
+        visitor: {
+            CallExpression(path, options) {
+                const customMessage = options.opts;
+                let defaultString = 'You are Awesome'
+                if(!customMessage || customMessage.message) {
+                    defaultString = customMessage.message;
+                }
+                if (path.node.callee.object && path.node.callee.object.name === 'console' && path.node.callee.property.name === 'log') {
+                    path.node.arguments.unshift({
+                        type: 'StringLiteral',
+                        value: `${defaultString}`
+                    })
+                }
+
+            }
+        }
+    };
+};
